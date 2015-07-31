75daf804d34a002631ab209ac270c8cbf406cec6
Roberto Ortega <berto.ort@gmail.com>
2015-07-30 23:42:24 -0600

    delete functional

--- routes/bookmarks.js
+++ routes/bookmarks.js
@@ -4,10 +4,8 @@ var db = require('./../models');
 
 router.get('/', function(req, res, next) {
   db.Users.findOne({_id: res.locals.userId}).then(function (user) {
-    console.log('here?');
     return db.Bookmarks.find({_id: {$in: user.bookmarks}})
   }).then(function (bookmarks) {
-    console.log('here');
     res.render('bookmarks/index', {bookmarks: bookmarks})
   })
 });
@@ -22,6 +20,32 @@ router.get('/:id', function (req, res, next) {
   })
 })
 
+router.get('/:id/edit', function (req, res, next) {
+  db.Bookmarks.findOne({_id: req.params.id}).then(function (bookmark) {
+    res.render('bookmarks/edit', {bookmark: bookmark})
+  })
+})
+
+router.get('/:id/delete', function (req, res, next) {
+  db.Bookmarks.findOneAndRemove({_id: req.params.id}).then(function () {
+    res.redirect('/users/' + res.locals.userId + '/bookmarks')
+  })
+})
+
+router.post('/:id/edit', function (req, res, next) {
+  var name = req.body.name;
+  var url = req.body.url;
+  var userId = res.locals.userId;
+  var description = req.body.description;
+  var type = req.body.type;
+  var categories = [];
+  db.Bookmarks.findByIdAndUpdate(req.params.id, {$set: {name: name, url: url,
+     userId: userId, description: description,
+     type: type, categories: categories}}).then(function (bookmark) {
+    res.redirect('/users/' + userId + '/bookmarks')
+  })
+})
+
 router.post('/new', function (req, res, next) {
   var name = req.body.name;
   var url = req.body.url;
@@ -32,7 +56,7 @@ router.post('/new', function (req, res, next) {
   db.Bookmarks.create({name: name, url: url,
      userId: userId, description: description,
      type: type, categories: categories}).then(function (bookmark) {
-      db.Users.findByIdAndUpdate(userId, {$push: {bookmarks: bookmark._id}}, {safe: true, upsert: false, new: true}, function (err, model) {
+      db.Users.findByIdAndUpdate(userId, {$push: {bookmarks: bookmark._id}}, {new: true}, function (err, model) {
       res.redirect('/users/' + userId + '/bookmarks')
     })
   });
