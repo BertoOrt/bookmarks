b440b82c1ee0e2f4c96185d73319b2488c24a203
Roberto Ortega <berto.ort@gmail.com>
2015-07-31 21:32:52 -0600

    looking better

--- models/index.js
+++ models/index.js
@@ -3,7 +3,3 @@ mongoose.connect("mongodb://" + process.env.MONGOLAB_URI);
 
 module.exports.Users = require('./users.js');
 module.exports.Bookmarks = require('./bookmarks.js');
-
-// array.forEach(function (element) {
-//   this.add(Card.apply(this, element))
-// }, this)
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/stylesheet', function(req, res, next) {
+  res.render('stylesheet');
+});
+
 module.exports = router;
