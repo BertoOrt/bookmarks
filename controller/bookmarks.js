a86054e55abbcc3fbe0f22fbb7305bafa1978cd4
Roberto Ortega <berto.ort@gmail.com>
2015-08-09 00:36:43 -0600

    bookmark controllers

--- controller/bookmarks.js
+++ controller/bookmarks.js
@@ -1,5 +1,6 @@
 var db = require('./../models');
 var bcrypt = require('bcryptjs');
+var logic = require('./../lib/logic.js');
 
 module.exports = {
 
@@ -7,8 +8,82 @@ module.exports = {
     return db.Users.findOne({_id: userId})
   },
 
-  findBookmark: function (user) {
-    return db.Bookmarks.find({_id: {$in: user.favorites}})
+  findBookmarks: function (user) {
+    return db.Bookmarks.find({_id: {$in: user.bookmarks}})
+  },
+
+  findBookmark: function (id) {
+    return db.Bookmarks.findOne({_id: id})
+  },
+
+  findUsers: function (bookmark) {
+    var promise = new Promise(function (resolve, reject) {
+      db.Users.find({favorites: String(bookmark._id)}).then(function (users) {
+        resolve({users:users, bookmark:bookmark})
+      })
+    })
+    return promise
+  },
+
+  favorite: function (users, id) {
+    var favorite = false;
+    users.forEach(function (user) {
+      if (String(user._id) === String(id)) {
+        favorite = true
+      }
+    })
+    return favorite
+  },
+
+  removeBookmark: function (id) {
+    return db.Bookmarks.findOneAndRemove({_id: id})
+  },
+
+  updateUser: function (bookmark) {
+    db.Users.update({_id: bookmark.userId}, {$pull: {bookmarks: bookmark._id}});
+    return bookmark;
+  },
+
+  updateCategories: function (bookmark) {
+    db.Categories.update({}, {$pull: {bookmarks: bookmark._id}}, {multi: true});
+    return bookmark;
+  },
+
+  editBookmark: function (name, url, userId, bookmarkId, description, categories) {
+    var promise = new Promise(function (resolve, reject) {
+      db.Bookmarks.findByIdAndUpdate(bookmarkId, {$set: {name: name, url: url,
+         userId: userId, description: description,
+         categories: categories}}).then(function (bookmark) {
+           resolve({bookmark:bookmark, categories: categories})
+         })
+    })
+    return promise;
+  },
+
+  allPromise: function (result) {
+    var promise = new Promise(function (resolve, reject) {
+      Promise.all(result.categories.map(function (category) {
+        return db.Categories.update({name: category}, {name: category, $push: {bookmarks: result.bookmark._id} }, {upsert: true})
+      })).then(function () {
+        resolve({bookmark: result.bookmark, userId: result.userId})
+      })
+    })
+    return promise
+  },
+
+  createBookmark: function (name, url, userId, description, categories) {
+    var promise = new Promise(function (resolve, reject) {
+      db.Bookmarks.create({name: name, url: url,
+        userId: userId, description: description,
+        categories: categories}).then(function (bookmark) {
+        resolve({bookmark: bookmark, categories: categories, userId: userId})
+      })
+    })
+    return promise;
+  },
+
+  updateUserBookmark: function (resolve) {
+    return db.Users.findByIdAndUpdate(resolve.userId, {$push: {bookmarks: resolve.bookmark._id}}, {new: true})
   }
 
 }
--- routes/bookmarks.js
+++ routes/bookmarks.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var db = require('./../models');
 var logic = require('./../lib/logic.js');
+var db = require('./../models');
 var auth = require('./../lib/auth');
+var route = require('./../controller/bookmarks');
+
 
 router.get('/', auth.authorizeUser, function(req, res, next) {
-  db.Users.findOne({_id: res.locals.userId}).then(function (user) {
-    return db.Bookmarks.find({_id: {$in: user.bookmarks}})
-  }).then(function (bookmarks) {
+  route.findUser(res.locals.userId).then(route.findBookmarks).then(function (bookmarks) {
     res.render('bookmarks/index', {bookmarks: bookmarks, cookieId: req.session.username})
   })
 });
@@ -17,77 +17,44 @@ router.get('/new', function (req, res, next) {
 })
 
 router.get('/:id', function (req, res, next) {
-  var bookmark, favorite;
-  db.Bookmarks.findOne({_id: req.params.id}).then(function (bm) {
-    bookmark = bm;
-    return db.Users.find({favorites: String(bookmark._id)})
-  }).then(function (data) {
-    data.forEach(function (user) {
-      if (String(user._id) === String(req.session.username)) {
-        favorite = true
-      }
-    })
-    res.render('bookmarks/show', {bookmark: bookmark, favorite: favorite, cookieId: req.session.username, categories: (logic.categories).sort()})
+  route.findBookmark(req.params.id).then(route.findUsers).then(function (result) {
+    var favorite = route.favorite(result.users, req.session.username);
+    res.render('bookmarks/show', {bookmark: result.bookmark, favorite: favorite, cookieId: req.session.username})
   })
 })
 
 router.get('/:id/edit', auth.authorizeUser, function (req, res, next) {
-  db.Bookmarks.findOne({_id: req.params.id}).then(function (bookmark) {
+  route.findBookmark(req.params.id).then(function (bookmark) {
     res.render('bookmarks/edit', {cookieId: req.session.username, bookmark: bookmark, categories: (logic.categories).sort()})
   })
 })
 
 router.get('/:id/delete', auth.authorizeUser, function (req, res, next) {
-  var bookmark;
-  db.Bookmarks.findOneAndRemove({_id: req.params.id}).then(function (bm) {
-    bookmark = bm;
-    return db.Users.update({_id: bookmark.userId}, {$pull: {bookmarks: bookmark._id}})
-  }).then(function () {
-    return db.Categories.update({}, {$pull: {bookmarks: bookmark._id}}, {multi: true})
-  }).then(function () {
+  route.removeBookmark(req.params.id).then(route.updateUser).then(route.updateCategories).then(function () {
     res.redirect('/users/' + req.session.username + '/bookmarks')
   })
 })
 
 router.post('/:id/edit', function (req, res, next) {
-  var bookmark;
   var name = req.body.name;
   var url = req.body.url;
-  var userId = res.locals.userId;
+  var userId = req.session.username;
   var description = req.body.description;
   var categories = logic.clean(req.body.categories.split(' '));
-  db.Bookmarks.findByIdAndUpdate(req.params.id, {$set: {name: name, url: url,
-     userId: userId, description: description,
-     categories: categories}}).then(function (bm) {
-    bookmark = bm;
-    return db.Categories.update({}, {$pull: {bookmarks: bookmark._id}}, {multi: true})
-  }).then(function () {
-    return Promise.all(categories.map(function (category) {
-      return db.Categories.update({name: category}, {name: category, $push: {bookmarks: bookmark._id} }, {upsert: true})
-    }))
-  }).then(function () {
-    res.redirect('/users/' + req.session.username + '/bookmarks')
+  var bookmarkId = req.params.id;
+  route.editBookmark(name, url, userId, bookmarkId, description, categories).then(route.allPromise).then(route.updateCategories).then(function () {
+    res.redirect('/users/' + userId + '/bookmarks')
   })
 })
 
 router.post('/new', function (req, res, next) {
-  var bookmark;
   var name = req.body.name;
   var url = req.body.url;
-  var userId = res.locals.userId;
+  var userId = req.session.username;
   var description = req.body.description;
   var categories = logic.clean(req.body.categories.split(' '));
-  db.Bookmarks.create({name: name, url: url,
-    userId: userId, description: description,
-    categories: categories}).then(function (bm) {
-      bookmark = bm;
-      return db.Users.findByIdAndUpdate(userId, {$push: {bookmarks: bookmark._id}}, {new: true})
-  }).then(function () {
-    return Promise.all(categories.map(function (category) {
-      return db.Categories.update({name: category}, {name: category, $push: {bookmarks: bookmark._id} }, {upsert: true})
-    }))
-  }).then(function () {
-    res.redirect('/users/' + req.session.username + '/bookmarks')
+  route.createBookmark(name, url, userId, description, categories).then(route.allPromise).then(route.updateUserBookmark).then(function () {
+    res.redirect('/users/' + userId + '/bookmarks')
   })
 })
 
